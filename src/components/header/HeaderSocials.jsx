4c7cb66972395079e3a7b1608764da3082adcbec
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-02-09 09:47:48 +0200

    added socials in the header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsYoutube } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/ademiralijagic/">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/Ade-mir">
+        <FaGithub />
+      </a>
+      <a href="https://www.youtube.com/@howtobecomeadeveloper">
+        <BsYoutube />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
