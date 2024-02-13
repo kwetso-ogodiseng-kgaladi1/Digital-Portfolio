1349258aac045fd2a0443a8a0b75d1c694709c90
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-02-13 10:58:19 +0200

    added correct socials links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { BsYoutube } from "react-icons/bs";
+// import { BsYoutube } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
